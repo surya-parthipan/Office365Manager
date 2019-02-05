37ecb4e0b5c04634c859e5f4b505d66a9b7dca30
Surya Parthipan <r.suryaparthipan@gmail.com>
2019-02-05 14:21:13 +0530

    Add files via upload

---
+++ Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Demo_API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
